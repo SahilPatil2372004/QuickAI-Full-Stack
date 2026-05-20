69791c7f180dc7ee52098828f2c59b07ccbf7fae
SahilPatil2372004 <sahilpatil2372004@gmail.com>
2026-05-20 15:00:35 +0530

    files added

---
+++ server/controllers/aiController.js
@@ -0,0 +1,134 @@
+import OpenAI from "openai";
+import sql from "../configs/db.js";
+import { clerkClient } from "@clerk/express";
+import axios from "axios";
+import fs from 'fs'
+import pdf from 'pdf-parse/lib/pdf-parse.js'
+
+const AI = new OpenAI({
+    apiKey: process.env.GEMINI_API_KEY,
+    baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/"
+});
+
+export const generateArticle = async (req, res)=>{
+    try {
+        const { userId } = req.auth();
+        const { prompt, length } = req.body;
+        const plan = req.plan;
+        const free_usage = req.free_usage;
+
+        if(plan !== 'premium' && free_usage >= 10){
+            return res.json({ success: false, message: "Limit reached. Upgrade to continue."})
+        }
+
+        const response = await AI.chat.completions.create({
+            model: "gemini-3-flash-preview",
+            messages: [{
+                    role: "user",
+                    content: prompt,
+                },
+            ],
+            temperature: 0.7,
+            max_tokens: 2500,
+        });
+
+        const content = response.choices[0].message.content
+
+        await sql` INSERT INTO creations (user_id, prompt, content, type) 
+        VALUES (${userId}, ${prompt}, ${content}, 'article')`;
+
+        if(plan !== 'premium'){
+            await clerkClient.users.updateUserMetadata(userId, {
+                privateMetadata:{
+                    free_usage: free_usage + 1
+                }
+            })
+        }
+
+        res.json({ success: true, content})
+
+
+    } catch (error) {
+        console.log(error.message)
+        res.json({success: false, message: error.message})
+    }
+}
+
+export const generateBlogTitle = async (req, res)=>{
+    try {
+        const { userId } = req.auth();
+        const { prompt } = req.body;
+        const plan = req.plan;
+        const free_usage = req.free_usage;
+
+        if(plan !== 'premium' && free_usage >= 10){
+            return res.json({ success: false, message: "Limit reached. Upgrade to continue."})
+        }
+
+        const response = await AI.chat.completions.create({
+            model: "gemini-3-flash-preview",
+            messages: [{ role: "user", content: prompt, } ],
+            temperature: 0.7,
+            max_tokens: 2500,
+        });
+
+        const content = response.choices[0].message.content
+
+        await sql` INSERT INTO creations (user_id, prompt, content, type) 
+        VALUES (${userId}, ${prompt}, ${content}, 'blog-title')`;
+
+        if(plan !== 'premium'){
+            await clerkClient.users.updateUserMetadata(userId, {
+                privateMetadata:{
+                    free_usage: free_usage + 1
+                }
+            })
+        }
+
+        res.json({ success: true, content})
+
+
+    } catch (error) {
+        console.log(error.message)
+        res.json({success: false, message: error.message})
+    }
+}
+
+export const resumeReview = async (req, res)=>{
+    try {
+        const { userId } = req.auth();
+        const resume = req.file;
+        const plan = req.plan;
+
+        if(plan !== 'premium'){
+            return res.json({ success: false, message: "This feature is only available for premium subscriptions"})
+        }
+
+        if(resume.size > 5 * 1024 * 1024){
+            return res.json({success: false, message: "Resume file size exceeds allowed size (5MB)."})
+        }
+
+        const dataBuffer = fs.readFileSync(resume.path)
+        const pdfData = await pdf(dataBuffer)
+
+        const prompt = `Review the following resume and provide constructive feedback on its strengths, weaknesses, and areas for improvement. Resume Content:\n\n${pdfData.text}`
+
+       const response = await AI.chat.completions.create({
+            model: "gemini-3-flash-preview",
+            messages: [{ role: "user", content: prompt, } ],
+            temperature: 0.7,
+            max_tokens: 3500,
+        });
+
+        const content = response.choices[0].message.content
+
+        await sql` INSERT INTO creations (user_id, prompt, content, type) 
+        VALUES (${userId}, 'Review the uploaded resume', ${content}, 'resume-review')`;
+
+        res.json({ success: true, content})
+
+    } catch (error) {
+        console.log(error.message)
+        res.json({success: false, message: error.message})
+    }
+}
